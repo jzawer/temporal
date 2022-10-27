7db5f777452c42ba065c6c2604f8925d65790215
Javier Otero Gonzalez <j.otero@innovation.es>
2022-10-27 11:12:32 +0200

    install primevue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import PrimeVue from 'primevue/config';
 import router from './router';
 
 import './assets/main.css';
+import './node_modules/primeflex/primeflex.css';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue);
 
 app.mount('#app');
